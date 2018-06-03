5331d86c2802d331088d5993aacb7f29527376b1
Stella-Ray-Chaves <stella@artyoga.net>
2018-06-03 15:39:44 -0700

    Enemey function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+var canvas = document.getElementById('canvas');
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speend = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
